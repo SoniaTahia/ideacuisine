d2383c5c6fef69ca47c4e392050cd7335cefc0a5
sonia <soniatahia@yahoo.fr>
2022-05-07 16:25:06 +0200

    rectif

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,15 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, User $user): Response
     {
         $user = $this->getUser();
         //     return $this->redirectToRoute('target_path');
