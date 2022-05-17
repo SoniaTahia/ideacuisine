af09fc7067754d0916f41d17173d2ba17b48cfda
sonia <soniatahia@yahoo.fr>
2022-05-17 10:29:21 +0200

    css update

--- migrations/Version20220516202405.php
+++ migrations/Version20220516202405.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220428235746 extends AbstractMigration
+final class Version20220516202405 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,7 +22,6 @@ final class Version20220428235746 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE invoice ADD CONSTRAINT FK_9065174455B7E546 FOREIGN KEY (invoice_user_id) REFERENCES user (id)');
         $this->addSql('CREATE INDEX IDX_9065174455B7E546 ON invoice (invoice_user_id)');
-        $this->addSql('ALTER TABLE product DROP slug');
     }
 
     public function down(Schema $schema): void
@@ -30,6 +29,5 @@ final class Version20220428235746 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE invoice DROP FOREIGN KEY FK_9065174455B7E546');
         $this->addSql('DROP INDEX IDX_9065174455B7E546 ON invoice');
-        $this->addSql('ALTER TABLE product ADD slug VARCHAR(255) NOT NULL');
     }
 }
